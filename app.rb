3217b7a9102350e78d89a74cf6ff7add8d335912
ALI ALRAWI <ali.developer90@gmail.com>
2016-11-30 15:22:29 +0000

    Done.

--- app.rb
+++ app.rb
@@ -9,6 +9,12 @@ end
 
 post '/' do
   text_from_user = params[:user_text]
+  ta=TextAnalyzer.new(text_from_user)
+  @vowels_count=at.count_of_vowels
+  @words_count=at.count_of_words
+  @contstant_count=at.count_of_consonants
+  @common_letter=at.most_used_letter
+
   erb :results
 end
 
