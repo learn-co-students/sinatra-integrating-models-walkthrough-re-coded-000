941397cad802065770c91d21b4665179b273f6a7
fatma yousef <famyaltimimi@yahoo.com>
2017-01-04 16:39:58 +0000

    Done.

--- models/textanalyzer.rb
+++ models/textanalyzer.rb
@@ -18,20 +18,20 @@ class TextAnalyzer
     @text.downcase.scan(/[bcdfghjklmnpqrstvwxyz]/).count
   end
 
-def most_used_letter
-    all_letters_in_string = @text.downcase.gsub(/[^a-z]/, '').split('') #gets rid of spaces and turns it into an array
-    letters_to_compare = all_letters_in_string.uniq
-    most_used_letter = ""
-    letter_count = 0
+ def most_used_letter
+     all_letters_in_string = @text.downcase.gsub(/[^a-z]/, '').split('') #gets rid of spaces and turns it into an array
+     letters_to_compare = all_letters_in_string.uniq
+     most_used_letter = ""
+     letter_count = 0
 
-    letters_to_compare.map do |letter|
-      letter_repetitions = all_letters_in_string.count(letter)
-      if letter_repetitions > letter_count
-        letter_count = letter_repetitions
-        most_used_letter = letter
-      end
-    end
-    biggest = [most_used_letter, letter_count]
-  end
+     letters_to_compare.map do |letter|
+       letter_repetitions = all_letters_in_string.count(letter)
+       if letter_repetitions > letter_count
+         letter_count = letter_repetitions
+         most_used_letter = letter
+       end
+     end
+     biggest = [most_used_letter, letter_count]
+   end
 
 end
